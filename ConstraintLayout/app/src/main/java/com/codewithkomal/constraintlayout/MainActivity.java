8e884c91e0a0ee7bee6ef872feb25148a814de86
Komal Ghafoor <mcsf19m041@pucit.edu.pk>
2021-05-29 08:14:21 -0700

    ConstraintLayout

---
+++ ConstraintLayout/app/src/main/java/com/codewithkomal/constraintlayout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.codewithkomal.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+    }
+}
